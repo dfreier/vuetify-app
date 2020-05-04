1ce58cea13a779f55df32d2d6ea9c5da87902a3c
Dominik Freier <dominik.freier@starmind.com>
2020-05-04 08:26:02 +0200

    Initial dist subtree commit

---
+++ dist/js/about.7cd92faf.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{4787:function(t,e,s){"use strict";s.r(e);var n=function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"pa-2 px-sm-8 py-sm-4 "},[s("QuestionsCard",{attrs:{title:!1,divider:""}})],1)},a=[],i=s("c0fa"),o={name:"Feed",components:{QuestionsCard:i["a"]}},r=o,u=s("2877"),c=Object(u["a"])(r,n,a,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=about.7cd92faf.js.map
