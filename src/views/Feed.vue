8b4c76bdb0d1e3e3ce4fafd32cfb901622bff396
Dominik Freier <dominik.freier@starmind.com>
2020-05-04 08:14:01 +0200

    wip

---
+++ src/views/Feed.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="pa-2 px-sm-8 py-sm-4 ">
+    <QuestionsCard :title="false" divider></QuestionsCard>
+  </div>
+</template>
+<script>
+    import QuestionsCard from "../components/questions/QuestionsCard";
+    export default {
+        name: 'Feed',
+        components: {
+            QuestionsCard
+        }
+    }
+</script>
